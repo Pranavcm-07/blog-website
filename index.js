8a866ec25dfe94d6ad1091b2534cb28c59253dde
pranav <pranavcm602@gmail.com>
2023-06-10 21:06:46 +0530

    added responsiveness to the website

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ const date = new Date()
 for (let i = 0; i<dateElement.length;i++){
     dateElement[i].innerText = date.toLocaleDateString()
 }
+document.getElementsByClassName('hamburger')[0].addEventListener('click',() => {
+    const clicked = document.getElementsByClassName('hamburger')[0].classList.contains('clicked')
+    if (clicked){
+        document.getElementsByClassName('hamburger')[0].innerHTML = '<i class="fa fa-bars" aria-hidden="true"></i>'
+        document.getElementsByClassName('hamburger')[0].classList.remove('clicked')
+    }else{
+        document.getElementsByClassName('hamburger')[0].innerHTML = '<i class="fa fa-times" aria-hidden="true"></i>'
+        document.getElementsByClassName('hamburger')[0].classList.add('clicked')
+    }
+})
